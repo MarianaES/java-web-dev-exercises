b4caaaf614b95405bbed58133dd1f8b4da0a7643
MarianaES <mariana.enriquez.s@gmail.com>
2021-01-09 14:38:59 -0600

    Move files to their corresponding chapter package

---
+++ src/exercises/ch4/Student.java
@@ -0,0 +1,51 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+// Start working here with your Student class.
+// To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+
+public class Student {
+
+    private String name;
+    private int studentId;
+    private int numberOfCredits = 0;
+    private double gpa = 0.0;
+
+    public Student (String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+
+    private void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
+}
---
+++ src/exercises/ch4/school/Teacher.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.demos.lsn3classes1.school;
+
+public class Teacher {
+}
