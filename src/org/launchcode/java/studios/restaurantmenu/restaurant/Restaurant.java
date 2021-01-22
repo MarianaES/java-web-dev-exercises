f4a628c97fd93433c1cdc0d36f9402668ab9174c
MarianaES <mariana.enriquez.s@gmail.com>
2021-01-21 20:37:49 -0600

    Add/remove menu items

--- src/org/launchcode/java/studios/restaurantmenu/restaurant/Restaurant.java
+++ src/org/launchcode/java/studios/restaurantmenu/restaurant/Restaurant.java
@@ -1,2 +1,25 @@
-package org.launchcode.java.studios.restaurantmenu.restaurant;public class Restaurant {
+package org.launchcode.java.studios.restaurantmenu.restaurant;
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+
+        MenuItem edamame = new MenuItem("Edamame", 5, "Steamed and salted soybeans", "Appetizers", false);
+        MenuItem ramen = new MenuItem("Ramen", 10, "Thin noodles, vegetables and broth", "Main course", false);
+        MenuItem tempura = new MenuItem("Cinnamon Tempura Gelato", 6, "Lightly battered cinnamon-swirl gelato, flash fried and finished with raspberry and chocolate sauces", "Dessert", true);
+
+        Menu menuDishes = new Menu();
+
+        menuDishes.addDish(edamame);
+        menuDishes.addDish(ramen);
+        menuDishes.addDish(tempura);
+
+        System.out.println(menuDishes.toString());
+        System.out.println(edamame.toString());
+
+        menuDishes.removeDish(ramen);
+        System.out.println(menuDishes.toString());
+
+    }
+
 }
